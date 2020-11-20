a060ac4c3e127214fbdd22351d4d2358a1318df8
Roman Yakoviv <r.yakoviv@pg-mc.com>
2020-11-20 14:32:01 +0200

    add swagger

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Phpro\ApiProblemBundle\ApiProblemBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
